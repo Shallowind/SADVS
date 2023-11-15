0e3b09d58a80e494a0bf8a33d4144cfcc3714725
zmmxkid <11922514+zmmxkid@user.noreply.gitee.com>
2023-11-15 15:55:48 +0800

    控制台

--- utils/myutil.py
+++ utils/myutil.py
@@ -1,4 +1,7 @@
 # 查验导入数据是否是图片
+from PyQt5.QtGui import QTextCursor
+
+
 def file_is_pic(suffix):
     if suffix == "png" or suffix == "jpg" or suffix == "bmp":
         return True
@@ -12,3 +15,20 @@ class Globals:
     dict_text = {}
     select_labels = []
     detection_run = False
+
+
+class ConsoleRedirector:
+    def __init__(self, parent, text_widget):
+        self.parent = parent
+        self.text_widget = text_widget
+
+    def write(self, text):
+        # 在控制台中显示文本
+        cursor = self.text_widget.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.text_widget.setTextCursor(cursor)
+        self.text_widget.ensureCursorVisible()
+
+    def flush(self):
+        pass
